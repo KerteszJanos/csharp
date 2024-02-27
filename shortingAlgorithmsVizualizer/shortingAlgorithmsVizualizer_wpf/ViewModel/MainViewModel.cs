db1cba50f41d4fd52e4e5069d24831aadc2c9a5c
Jani <k.janos.jani@gmail.com>
2024-02-27 20:24:25 +0100

    made viewmodel and Command class

---
+++ shortingAlgorithmsVizualizer/shortingAlgorithmsVizualizer_wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace shortingAlgorithmsVizualizer_wpf.ViewModel
+{
+    public class MainViewModel
+    {
+        #region Propertis
+
+        //Commands
+        public ICommand? ExitCommand { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            ExitCommand = new Commands.Commands(Exit, CanExit);
+        }
+        #endregion
+
+        #region Command methods
+        private bool CanExit(object obj)
+        {
+            return true;
+        }
+
+        private void Exit(object obj) //the logic what we want to execute when the command is invoked
+        {
+            //exit the app
+        }
+        #endregion
+    }
+}
